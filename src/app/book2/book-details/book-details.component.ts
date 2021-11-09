80702c328797ddbe4d320f4bce80ccfc328e9d41
Luke Sweeney <lukesweeney00@gmail.com>
2021-11-09 19:39:13 +0000

    Added components to pass data from parent to child

---
+++ src/app/book2/book-details/book-details.component.ts
@@ -0,0 +1,18 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/book';
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent implements OnInit {
+  @Input() book!: Book;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/book2/book-row/book-row.component.ts
@@ -0,0 +1,20 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/book';
+
+@Component({
+  selector: 'app-book-row',
+  templateUrl: './book-row.component.html',
+  styleUrls: ['./book-row.component.css']
+})
+export class BookRowComponent implements OnInit {
+  @Input() book!: Book;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
